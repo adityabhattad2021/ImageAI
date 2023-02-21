48e10ff3c8e15fc28d7fa8b149db8c6675f5602c
Aditya Bhattad <adityabhattad18@gmail.com>
2023-02-21 21:24:51 +0530

    Project Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+	Link,
+	Route
+} from "react-router-dom";
+import {
+	Home,
+	CreatePost
+} from "./pages"
+
+export default function App() {
+	return <div>Home</div>;
+}
