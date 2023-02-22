44027f3adf51888c8b6a89384f169e089028a1c1
Aditya Bhattad <adityabhattad18@gmail.com>
2023-02-22 11:12:14 +0530

    Image Generation Route Setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express, { response } from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./mongodb/connect";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +11,9 @@ const PORT = 8080;
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post",postRoutes);
+app.use("/api/v1/dalle",dalleRoutes);
 
 app.get("/", async (request, response) => {
 	response.send("Hello from backend...");
